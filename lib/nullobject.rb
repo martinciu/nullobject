0607722314f1211b2df93f2eb235b305b04963d3
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2011-11-19 13:44:34 +0100

    removed code that shouldn't be here

--- lib/nullobject.rb
+++ lib/nullobject.rb
@@ -4,42 +4,16 @@ module Null
 
   def self.included(base)
     base.send(:include, Singleton)
-    base.extend Null::ClassMethods
   end
 
   def to_a; []; end
-  alias :to_ary :to_a
   def to_s; ""; end
-  alias :to_str :to_s
   def to_f; 0.0; end
   def to_i; 0; end
   def nil?; true; end
-  def +(other); other; end
-  def -(other); coerced(other) - other; end
-  
-  def coerce(other)
-    [other, coerced(other)]
-  end
 
   def method_missing(*args, &block)
     self
-  end
-
-  protected
-    def coerced(other)
-      send({
-        Integer => :to_i,
-        Fixnum  => :to_i,
-        String  => :to_s,
-        Float   => :to_f,
-        Array   => :to_a
-      }[other.class])
-    end
-
-  module ClassMethods
-    def maybe(value)
-      value.nil? ? instance : value
-    end
   end
 
   class Object
--- spec/lib/nullobject_spec.rb
+++ spec/lib/nullobject_spec.rb
@@ -11,10 +11,6 @@ describe "Null::Object" do
       @it.to_s.must_equal ""
     end
 
-    it "#to_str return empty string" do
-      @it.to_str.must_equal ""
-    end
-
     it "#to_a return empty array" do
       @it.to_a.must_equal []
     end
@@ -29,55 +25,6 @@ describe "Null::Object" do
 
     it "#nil? return true" do
       @it.nil?.must_equal true
-    end
-  
-    describe "#+" do
-      it "does not modify integer" do
-        (@it + 5).must_equal 5
-        (5 + @it).must_equal 5
-      end
-      
-      it "does not modify float" do
-        (@it + 5.0).must_equal 5.0
-        (5.0 + @it).must_equal 5.0
-      end
-      
-      it "does not modify string" do
-        (@it + "foo").must_equal "foo"
-        ("foo" + @it).must_equal "foo"
-      end
-
-      it "does not modify array" do
-        (@it + [1, 3, "foo"]).must_equal [1, 3, "foo"]
-        ([1, 3, "foo"] + @it).must_equal [1, 3, "foo"]
-      end
-    end
-
-    describe "#-" do
-      it "does not modify integer" do
-        (5 - @it).must_equal 5
-      end
-
-      it "acts as empty if other substracts from" do
-        (@it - 5).must_equal -5
-        (@it - [5]).must_equal []
-      end
-
-      it "does not modify array" do
-        ([1, 3, "foo"] - @it).must_equal [1, 3, "foo"]
-      end
-    end
-
-  end
-
-  describe "#maybe" do
-    it "returns NullObject if value is nil" do
-      Null::Object.maybe(nil).must_be_kind_of Null::Object
-    end
-
-    it "returns value if it is not nil" do
-      val = "foo"
-      Null::Object.maybe("foo").must_equal val
     end
   end
 
@@ -96,10 +43,6 @@ describe "Null::Object" do
 
     it "#to_s return empty string" do
       @it.to_s.must_equal ""
-    end
-
-    it "#to_str return empty string" do
-      @it.to_str.must_equal ""
     end
 
     it "#to_a return empty array" do
@@ -122,43 +65,6 @@ describe "Null::Object" do
       @it.to_bar.must_equal "nothing here"
     end
   
-    describe "#+" do
-      it "does not modify integer" do
-        (@it + 5).must_equal 5
-        (5 + @it).must_equal 5
-      end
-      
-      it "does not modify float" do
-        (@it + 5.0).must_equal 5.0
-        (5.0 + @it).must_equal 5.0
-      end
-      
-      it "does not modify string" do
-        (@it + "foo").must_equal "foo"
-        ("foo" + @it).must_equal "foo"
-      end
-
-      it "does not modify array" do
-        (@it + [1, 3, "foo"]).must_equal [1, 3, "foo"]
-        ([1, 3, "foo"] + @it).must_equal [1, 3, "foo"]
-      end
-    end
-
-    describe "#-" do
-      it "does not modify integer" do
-        (5 - @it).must_equal 5
-      end
-
-      it "acts as empty if other substracts from" do
-        (@it - 5).must_equal -5
-        (@it - [5]).must_equal []
-      end
-
-      it "does not modify array" do
-        ([1, 3, "foo"] - @it).must_equal [1, 3, "foo"]
-      end
-    end
-
   end
 
 end
